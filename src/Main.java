ce8bd99697800074e2c144fb6ae93d4217c12902
Admin <kundyz.mus@mail.ru>
2025-03-30 00:22:35 +0500

    Problem 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -10,28 +11,7 @@ public class Main {
             arr[i] = input.nextInt();
         }
 
-        System.out.println(findMinimum(n, arr));
-    }
-
-    /**
-     * This method finds the minimum element in an array.
-     * It uses an iterative approach with a for loop.
-
-     * Time complexity: O(n), where n is the number of elements in the array.
-     * The algorithm iterates through all elements once, resulting in linear time complexity.
-
-     * @param n   The number of elements in the array.
-     * @param arr The array from which the minimum value is to be found.
-     * @return The minimum element in the given array.
-     */
-    public static int findMinimum(int n, int[] arr) {
-        int min = arr[0];
-
-        for (int i = 1; i < n; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
-            }
-        }
-        return min;
+        MinimumOftheArray finder = new MinimumOftheArray(arr);
+        System.out.println(finder.findMinimum());
     }
 }
---
+++ src/MinimumOftheArray.java
@@ -0,0 +1,27 @@
+class MinimumOftheArray {
+    private int[] arr;
+
+    public MinimumOftheArray(int[] arr) {
+        this.arr = arr;
+    }
+
+    /**
+     * This method finds the minimum element in an array.
+     * It uses an iterative approach with a for loop.
+     *
+     * Time complexity: O(n), where n is the number of elements in the array.
+     * The algorithm iterates through all elements once, resulting in linear time complexity.
+     *
+     * @return The minimum element in the given array.
+     */
+    public int findMinimum() {
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
